c082f44e1a3bbc6e867e53d0ed293077f5750d25
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-09-27 01:24:15 -0400

    Update a2 kambaz

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,7 +1,98 @@
-export default function People() {
-    return (
-        <div id="wd-people">
-            <h3>People</h3>
-        </div>
-    );
+import { Table } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+export default function PeopleTable() {
+ return (
+  <div id="wd-people-table">
+   <Table striped>
+        <thead>
+     <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Tony</span>{" "}
+        <span className="wd-last-name">Stark</span>
+      </td>
+      <td className="wd-login-id">001234561S</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-01</td>
+      <td className="wd-total-activity">10:21:32</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Bruce</span>{" "}
+        <span className="wd-last-name">Wayne</span>
+      </td>
+      <td className="wd-login-id">001234562B</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-03</td>
+      <td className="wd-total-activity">08:15:10</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Steve</span>{" "}
+        <span className="wd-last-name">Rogers</span>
+      </td>
+      <td className="wd-login-id">001234563S</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-05</td>
+      <td className="wd-total-activity">12:42:05</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Natasha</span>{" "}
+        <span className="wd-last-name">Romanoff</span>
+      </td>
+      <td className="wd-login-id">001234564N</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-07</td>
+      <td className="wd-total-activity">09:33:47</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Peter</span>{" "}
+        <span className="wd-last-name">Parker</span>
+      </td>
+      <td className="wd-login-id">001234565P</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-09</td>
+      <td className="wd-total-activity">07:28:19</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Diana</span>{" "}
+        <span className="wd-last-name">Prince</span>
+      </td>
+      <td className="wd-login-id">001234566D</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">TA</td>
+      <td className="wd-last-activity">2020-10-10</td>
+      <td className="wd-total-activity">14:05:54</td>
+    </tr>
+    <tr>
+      <td className="wd-full-name text-nowrap">
+        <FaUserCircle className="me-2 fs-1 text-secondary" />
+        <span className="wd-first-name">Clark</span>{" "}
+        <span className="wd-last-name">Kent</span>
+      </td>
+      <td className="wd-login-id">001234567C</td>
+      <td className="wd-section">S101</td>
+      <td className="wd-role">STUDENT</td>
+      <td className="wd-last-activity">2020-10-12</td>
+      <td className="wd-total-activity">11:17:02</td>
+    </tr>
+    </tbody>
+   </Table>
+  </div> );
 }
--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,77 +1,104 @@
 import Link from "next/link";
-import Image from "next/image";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/esm/Card";
+import CardBody from "react-bootstrap/esm/CardBody";
+import Button from "react-bootstrap/esm/Button";
+import CardText from "react-bootstrap/esm/CardText";
+import CardTitle from "react-bootstrap/esm/CardTitle";
+import CardImg from "react-bootstrap/esm/CardImg";
 export default function Dashboard() {
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
             <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
             <div id="wd-dashboard-courses">
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1234" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1234 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1235" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1235 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1236" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1236 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1237" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1237 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1238" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1238 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
-                <div className="wd-dashboard-course">
-                    <Link href="/Courses/1239" className="wd-dashboard-course-link">
-                        <Image src="/images/reactjs.jpg" alt="React JS course cover" width={200} height={150} />
-                        <div>
-                            <h5> CS1239 React JS </h5>
-                            <p className="wd-dashboard-course-title">
-                                Full Stack software developer{" "}</p>
-                            <button> Go </button>
-                        </div>
-                    </Link>
-                </div>
+                <Row xs={1} md={5} className="g-4">
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1234/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1235/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1235 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1236/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1236 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1237/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1237 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1238/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1238 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href="/Courses/1239/Home"
+                                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1239 React JS</CardTitle>
+                                    <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        Full Stack software developer</CardText>
+                                    <Button variant="primary">Go</Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         </div>
 );
