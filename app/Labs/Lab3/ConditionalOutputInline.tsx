85c33523df2c5fdec6a69e04aaa462514cff105f
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-10-13 00:35:21 -0400

    update a3

--- app/Labs/Lab3/ConditionalOutputInline.tsx
+++ app/Labs/Lab3/ConditionalOutputInline.tsx
@@ -3,7 +3,7 @@ export default function ConditionalOutputInline() {
     return (
       <div id="wd-conditional-output-inline">
         {loggedIn && <h2>Welcome Inline</h2>}
-        {!loggedIn && <h2>Please login Inline</h2>}
+        {!loggedIn && <h2>Please login Inline</h2>}<hr/>
       </div>
    );
 }
