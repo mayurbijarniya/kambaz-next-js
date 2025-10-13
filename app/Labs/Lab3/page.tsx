77390f5046ebe8478cbc49e0dd11db9af857b053
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-10-12 22:14:16 -0400

    update a3

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -9,6 +9,20 @@ import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
 import ImpliedReturn from "./ImpliedReturn";
 import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import Spreader from "./Spreader";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -24,6 +38,20 @@ export default function Lab3() {
       <ArrowFunctions />
       <ImpliedReturn />
       <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House /> 
+      <Spreader />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
     </div>
   );
 }
