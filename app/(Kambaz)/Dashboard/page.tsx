be9ab7595deadb07f6c2fa50bd81475168eb9b72
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-10-20 21:43:47 -0400

    update a4 assignment Dashboard

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
@@ -7,27 +10,154 @@ import Button from "react-bootstrap/esm/Button";
 import CardText from "react-bootstrap/esm/CardText";
 import CardTitle from "react-bootstrap/esm/CardTitle";
 import CardImg from "react-bootstrap/esm/CardImg";
+import FormControl from "react-bootstrap/esm/FormControl";
 import * as db from "../Database";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Dashboard() {
-    const courses = db.courses;
-    return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+  const [courses, setCourses] = useState<any[]>(db.courses);
+
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse]);
+  };
+
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((c) => c._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+  return (
+    <div id="wd-dashboard" className="p-4">
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+
+      <h5>
+        New Course
+        <Button
+          className="float-end"
+          variant="primary"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          Add
+        </Button>
+        <Button
+          className="float-end me-2"
+          variant="warning"
+          id="wd-update-course-click"
+          onClick={updateCourse}
+        >
+          Update
+        </Button>
+      </h5>
+      <br />
+
+      <FormControl
+        value={course.name}
+        placeholder="Course Name"
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+
+      <FormControl
+        value={course.number}
+        placeholder="Course Number"
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+
+      <FormControl
+        value={course.description}
+        placeholder="Course Description"
+        as="textarea"
+        rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <hr />
+
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
+      <hr />
+
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
           {courses.map((course) => (
-            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Col
+              key={course._id}
+              className="wd-dashboard-course"
+              style={{ width: "300px" }}
+            >
               <Card>
-                <Link href={`/Courses/${course._id}/Home`}
-                      className="wd-dashboard-course-link text-decoration-none text-dark" >
-                  <CardImg src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
+                <Link
+                  href={`/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    src="/images/reactjs.jpg"
+                    variant="top"
+                    width="100%"
+                    height={160}
+                  />
                   <CardBody className="card-body">
                     <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                      {course.name} </CardTitle>
-                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                      {course.description} </CardText>
-                    <Button variant="primary"> Go </Button>
+                      {course.name}
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      {course.description}
+                    </CardText>
+
+                    <div className="d-flex justify-content-between align-items-center">
+                      <Button variant="primary">Go</Button>
+
+                      <div>
+                        <Button
+                          variant="warning"
+                          className="me-2"
+                          id="wd-edit-course-click"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            setCourse(course);
+                          }}
+                        >
+                          Edit
+                        </Button>
+
+                        <Button
+                          variant="danger"
+                          id="wd-delete-course-click"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            deleteCourse(course._id);
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    </div>
                   </CardBody>
                 </Link>
               </Card>
@@ -36,5 +166,5 @@ export default function Dashboard() {
         </Row>
       </div>
     </div>
-    );
+  );
 }
