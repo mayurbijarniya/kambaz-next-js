975aef5221f84d7e9f2105ab2bcad41c019917aa
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-13 22:58:37 -0500

    a6

--- app/(Kambaz)/Courses/[cid]/People/Table.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table.tsx
@@ -1,11 +1,25 @@
 "use client"
 
+import { useState } from "react";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
 
 export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
+
   return (
     <div id="wd-people-table">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }}
+        />
+      )}
       <Table className="table table-striped">
         <thead>
           <tr>
@@ -21,9 +35,18 @@ export default function PeopleTable({ users = [], fetchUsers }: { users?: any[];
           {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name text-danger">{user.firstName}</span>{" "}
-                <span className="wd-last-name text-danger">{user.lastName}</span>
+                <span
+                  className="text-decoration-none"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    setShowDetails(true);
+                    setShowUserId(user._id);
+                  }}
+                >
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name text-danger">{user.firstName}</span>{" "}
+                  <span className="wd-last-name text-danger">{user.lastName}</span>
+                </span>
               </td>
               <td className="wd-login-id">{user.loginId || user.username}</td>
               <td className="wd-section">{user.section || "N/A"}</td>
